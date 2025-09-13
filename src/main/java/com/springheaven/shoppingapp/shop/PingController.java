eaab79583d1b9e0b3417027adeca35e85764073e
praveenraj <impraveen13@gmail.com>
2025-09-13 20:05:11 +0200

    "removed redundant code"

--- src/main/java/com/springheaven/shoppingapp/shop/PingController.java
+++
@@ -1,9 +0,0 @@
-package com.springheaven.shoppingapp.shop;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class PingController {
-    @GetMapping("/ping") public String ping() { return "ok"; }
-}
